e64e1425de51fe3c593dfccd4262065d29b32f51
wondervictor <victorchanchina@gmail.com>
2017-06-09 15:21:35 +0800

    add common

---
+++ include/Common.h
@@ -0,0 +1,15 @@
+//
+// Created by VicChan on 2017/6/9.
+//
+
+#ifndef MEMSTEVE_COMMON_H
+#define MEMSTEVE_COMMON_H
+
+// unit: u seconds
+typedef unsigned int timeInterval;
+
+void startTimer();
+
+void stopTimer(timeInterval& duration);
+
+#endif //MEMSTEVE_COMMON_H
---
+++ src/Common.cpp
@@ -0,0 +1,15 @@
+//
+// Created by VicChan on 2017/6/9.
+//
+
+#include "../include/Common.h"
+
+
+void startTimer() {
+
+}
+
+
+void stopTimer(timeInterval& duration) {
+
+}
